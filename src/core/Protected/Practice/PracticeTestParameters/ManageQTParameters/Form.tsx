ac58437b6dd69ab498ec79f8a2d6e7e23d6bca9a
Subharaj Chaudhary <subharazzchy68@gmail.com>
2022-03-31 12:58:52 +0545

    Create Form

--- src/core/Protected/Practice/PracticeTestParameters/ManageQTParameters/Form.tsx
+++ src/core/Protected/Practice/PracticeTestParameters/ManageQTParameters/Form.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { Button } from 'reactstrap';
 
 const Form = () => {
     return (
@@ -10,7 +11,7 @@ const Form = () => {
                             <label htmlFor="" className="mr-1">
                                 Parameters Name
                             </label>
-                            <input></input>
+                            <input type='text' id='parameter_name' className='form-control'/>
                         </div>
                     </div>
                     <div className='col-md-3'>
@@ -18,7 +19,7 @@ const Form = () => {
                             <label htmlFor='' className='mr-1'>
                                 Unit:
                             </label>
-                            <input></input>
+                            <input className='form-control'/>
                         </div>
                     </div>
                     <div className='col-md-3'>
@@ -26,8 +27,31 @@ const Form = () => {
                             <label htmlFor='' className='mr-1'>
                                 NDWQ Standard:
                             </label>
-                            <input></input>
+                            <input className='form-control'/>
                         </div>
+                    </div>
+                    <div className='col-md-3'>
+                        <div className='form-group'>
+                            <label htmlFor='' className='mr-1'>
+                                Types
+                            </label>
+                            <select className='form-control'>
+                                <option>Chemical</option>
+                                <option>Others</option>
+                            </select>
+                        </div>
+                    </div>
+                    
+                    <div className='col-md-12 text-right'>
+                        <Button type='submit' className='btn btn-primary' color='primary' 
+                            style={{padding: "8px 15px",
+                                    borderRadius: "5px", 
+                                    fontWeight: "bold", 
+                                    letterSpacing:"2px", 
+                                    fontSize: "0.9rem"}}
+                        >
+                            SAVE
+                        </Button>
                     </div>
                 </div>
             </form>
