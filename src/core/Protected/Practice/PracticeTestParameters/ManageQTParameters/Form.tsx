b74dafd7476a49de03d9730174387216dd03f6f8
Subharaj Chaudhary <subharazzchy68@gmail.com>
2022-04-11 15:00:06 +0545

    update select text

--- src/core/Protected/Practice/PracticeTestParameters/ManageQTParameters/Form.tsx
+++ src/core/Protected/Practice/PracticeTestParameters/ManageQTParameters/Form.tsx
@@ -13,6 +13,7 @@ import { useTranslation } from "react-i18next";
 import { PARAMETER_TYPES_OPTIONS } from "constants/constants";
 import StyledSelect from "components/React/StyledSelect/StyledSelect";
 import TooltipLabel from "components/UI/TooltipLabel";
+import './forms.scss';
 
 interface Props extends PropsFromRedux {
   editData: any;
