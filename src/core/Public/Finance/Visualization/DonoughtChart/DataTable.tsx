de52d33ce14211aa3563b5e103711ac5790456f4
Subharaj Chaudhary <subharazzchy68@gmail.com>
2022-04-18 15:04:36 +0545

    update en to ne

--- src/core/Public/Finance/Visualization/DonoughtChart/DataTable.tsx
+++ src/core/Public/Finance/Visualization/DonoughtChart/DataTable.tsx
@@ -1,5 +1,6 @@
 import { getNumberByLanguage } from "i18n/i18n";
 import React from "react";
+import { useTranslation } from "react-i18next";
 import { useSelector } from "react-redux";
 import { Table } from "reactstrap";
 import { RootState } from "store/root-reducer";
@@ -10,6 +11,8 @@ interface Props {
 }
 
 const DataTable = (props: Props) => { 
+  
+  const { t } = useTranslation();
 
   const currency = useSelector((state: RootState) => state.waterSchemeData.waterSchemeDetailsData.data?.currency)
 
@@ -28,8 +31,8 @@ const DataTable = (props: Props) => {
             {props.tableData?.map((data) => (
               <tr>
                 <td>{data.name}</td>
-                <td>{currency} {getNumberByLanguage(data.value.this_year)}</td>
-                <td>{currency} {getNumberByLanguage(data.value.all_time)}</td>
+                <td>{t("home:rs")} {getNumberByLanguage(data.value.this_year)}</td>
+                <td>{t("home:rs")} {getNumberByLanguage(data.value.all_time)}</td>
               </tr>
             ))}
           </tbody>
